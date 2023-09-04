f91cbd5d2cf7f52fd5158808b74aa804b306a1b3
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2023-09-04 14:14:55 +0200

    Add custom query execution support

--- src/main/java/com/example/spring/data/fhir/query/FHIRQuery.java
+++ src/main/java/com/example/spring/data/fhir/query/FHIRQuery.java
@@ -1,6 +1,7 @@
 package com.example.spring.data.fhir.query;
 
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import com.example.spring.data.fhir.annotations.Query;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.query.QueryMethod;
@@ -35,12 +36,37 @@ public class FHIRQuery implements RepositoryQuery {
     private void extractParametersNames(Method method, RepositoryMetadata metadata) {
         var queryName = method.getName();
         if (method.getParameterCount() > 0) {
-            PartTree tree = new PartTree(queryName, metadata.getReturnedDomainClass(method));
-            if (tree.iterator().hasNext()) {
-                for (PartTree.OrPart orPart : tree) {
-                    for (Part part : orPart) {
-                        paramNames.add(part.getProperty().getLeafProperty().getSegment());
-                    }
+            Query annotation = method.getDeclaredAnnotation(Query.class);
+
+            if (annotation != null) {
+                extractFromQuery(annotation.value());
+            } else {
+                extractFromMethodName(method, metadata, queryName);
+            }
+        }
+    }
+
+    private void extractFromQuery(String query) {
+        // Query validation can be added here...
+        // Selected fields can be parsed here...
+        // Sorting logic and pagination can be parsed here...
+
+        String[] queryParts = query.split("WHERE", 2);
+        String[] filters = queryParts[1].split("AND");
+        for (String filter : filters) {
+            String[] filterParts = filter.split("=");
+            if (filterParts[1].contains(":")) {
+                paramNames.add(filterParts[0].trim());
+            }
+        }
+    }
+
+    private void extractFromMethodName(Method method, RepositoryMetadata metadata, String queryName) {
+        PartTree tree = new PartTree(queryName, metadata.getReturnedDomainClass(method));
+        if (tree.iterator().hasNext()) {
+            for (PartTree.OrPart orPart : tree) {
+                for (Part part : orPart) {
+                    paramNames.add(part.getProperty().getLeafProperty().getSegment());
                 }
             }
         }
